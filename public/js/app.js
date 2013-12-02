da990091975a041840961738a84bac59385ca679
Salome Gonzalez <alu0100507041@ull.edu.es>
2013-12-02 15:12:38 +0000

    Añadiendo el javascript

--- public/js/app.js
+++ public/js/app.js
@@ -5,3 +5,30 @@ $(function() {
   });
 });
 */
+
+$(document).ready(function(){
+    $(".cell").click(function(event) {
+        var pathname = window.location.pathname;
+        var ruta = pathname + event.target.id;
+        $.get(ruta, function(data) {
+            if (data == 'illegal'){
+                alert("Este movimiento es ilegal");
+                }
+            else if (data.length < 3) {
+                if (data.length > 0){
+                    $("#"+event.target.id).addClass("circle");
+                    $("#"+data).addClass( "cross" );
+                }
+                else{
+                    alert("Esta casilla esta ocupada por lo que  debe escoger otra");
+                }
+            }
+            else{ 
+                setTimeout(function(){
+                url = data;
+                $(location).attr('href',url);
+                },200);
+            }
+        });
+    });
+});
